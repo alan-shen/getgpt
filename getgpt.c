56a5dc5fc25fe0c5e4e34e78e7126b1f3d6a3e27
b645 <b645@borqs.com>
2014-02-14 11:30:10 +0800

    Can read and show the gpt partitiion info now

--- getgpt.c
+++ getgpt.c
@@ -4,55 +4,121 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/system_properties.h>
-#include <cutils/properties.h>
 
-int getacd_string(uint8_t index)
-{
-        unsigned char *ChaabiDataAddr = NULL;
-        int length = get_customer_data( index, (void ** const) &ChaabiDataAddr);
-	int i;
+#define MMCDEV 		"/dev/block/mmcblk0"
+#define BLOCKSIZE	(512)
+#define GPTTABLESIZE	(128)
 
-	printf("\tCHAABI Read ACD Index %d, Count: %4d, Data: %s", index, length, ChaabiDataAddr);
-	printf("\n");
+#define MBR_OFFSET		(0)
+#define GPTHEADER_OFFSET	(1)
 
-	return 0;
-}
-
-int getacd_hex(uint8_t index)
-{
-        unsigned char *ChaabiDataAddr = NULL;
-        int length = get_customer_data( index, (void ** const) &ChaabiDataAddr);
-	int i;
-
-	printf("\tCHAABI Read ACD Index %d, Count: %4d, Data: ", index, length);
-	for( i=0; i<length; i++){
-		printf("%02x", ChaabiDataAddr[i]);
-		if( i != length-1){
-			printf(":");
-		}
-	}
-	printf("\n");
-
-#if 0
-	int i, j;
-	char vBTMac[20];
-	for ( i=0,j=0; i<BTMAC_ADDRESS_LEN*2,j<BTMAC_ADDRESS_LEN; i+=2,j++ ){
-		sprintf( &vBTMac[i],   "%x", (ChaabiBTMacAddr[j]&0xf0)>>4 );
-		sprintf( &vBTMac[i+1], "%x", (ChaabiBTMacAddr[j]&0x0f) );
-		printf("%02x:%c%c\n", ChaabiBTMacAddr[j], vBTMac[i], vBTMac[i+1]  );
-	}	
-#endif
-
-	return 0;
-}
+#define MBRDAT				"/data/partition_mbr.dat"
+#define PRIMARYGPTHEADERDAT		"/data/partition_gptheader.dat"
+#define GPTTABLEDAT			"/data/partition_gpttable.dat"
 
 int main(int argc, char **args)
 {
-	getacd_hex( ACD_BT_MAC_ADDR_FIELD_INDEX );
-	getacd_hex( ACD_WLAN_MAC_ADDR_FIELD_INDEX );
+	int fd_mmc;
+	char buffer[BLOCKSIZE];
+	int i, j;
+	unsigned int lba_start, lba_end, psize;
+	char part_name[80];
 
-	getacd_string( ACD_CUSTOMER_RESERVED_15_FIELD_INDEX );
+	fd_mmc = open(MMCDEV, O_RDWR);
+	if( fd_mmc < 0 ){
+		return -1;
+	}
+
+	if(0){
+		printf("===== MBR =====\n");
+		memset( buffer, 0, BLOCKSIZE);
+		//seek( fd_mmc, MBR_OFFSET, SEEK_SET );
+		read( fd_mmc, buffer, BLOCKSIZE );
+		for( i=0; i<BLOCKSIZE; i++ ){
+			printf("%02X", buffer[i]&0xFF);
+			printf(" ");
+			if( (i!=0) && (i%16==15) ){
+				printf("\n");
+			}
+		}
+		printf("\n");
+	}
+
+	if(0){
+		printf("===== GPT HEADER =====\n");
+		memset( buffer, 0, BLOCKSIZE);
+		//seek( fd_mmc, GPTHEADER_OFFSET, SEEK_SET );
+		read(fd_mmc, buffer, BLOCKSIZE );
+		for( i=0; i<BLOCKSIZE; i++ ){
+			printf("%02X", buffer[i]&0xFF);
+			printf(" ");
+			if( (i!=0) && (i%16==15) ){
+				printf("\n");
+			}
+		}
+		printf("\n");
+	}
+
+	//printf("===== GPT TABLE =====\n");
+	printf("---------------------------------------------------------------\n");
+	printf("START_LBA,    END_LBA,         LBA -    SIZE(MB) - NAME\n");
+	printf("---------------------------------------------------------------\n");
+	/* skip 2 block */
+	read( fd_mmc, buffer, BLOCKSIZE );
+	read( fd_mmc, buffer, BLOCKSIZE );
+	/* skip 2 block */
+	while(1){
+		/* Read ont partition info, which size is 128 Bytes */
+		memset( buffer, 0, BLOCKSIZE);
+		read(fd_mmc, buffer, GPTTABLESIZE );
+
+	#if 0
+		/* Show the original data */
+		for( i=0; i<GPTTABLESIZE; i++ ){
+			printf("%02X", buffer[i]&0xFF);
+			printf(" ");
+			if( (i!=0) && (i%16==15) ){
+				printf("\n");
+			}
+		}
+	#endif
+
+		/* Check if this is the last partition */
+		for(i=0; i<32; i++){
+			if( buffer[i] != 0 ){
+				break;
+			}
+		}
+		if(i==32){
+			break;
+		}
+
+		/* Check the partition Name */
+		memset( part_name, 0, sizeof(part_name));
+		for(i=0; i<72; i++){
+			part_name[i] = buffer[56+i*2]&0xFF;
+		}
+
+		/* Check the start block address */
+		lba_start &= 0x00000000;
+		for( i=4; i<8; i++ ){
+			//printf("%02X ", buffer[32+8-1-i]&0xFF);
+			lba_start |= ((buffer[32+8-1-i]&0xFF)<<((8-1-i)*2*4));
+		}
+
+		/* Check the   end block address */
+		lba_end &= 0x00000000;
+		for( i=4; i<8; i++ ){
+			//printf("%02X ", buffer[32+8+8-1-i]&0xFF);
+			lba_end |= ((buffer[32+8+8-1-i]&0xFF)<<((8-1-i)*2*4));
+		}
+
+		/* caluate the partition size of MB */
+		psize = (lba_end - lba_start)/2/1024;
+
+		/* Show partition info */
+		printf("0x%08X, 0x%08X, %10d - %10dM - (%s)\n", lba_start, lba_end, (lba_end-lba_start), psize+1, part_name);
+	}
 
         return 0;
 }
