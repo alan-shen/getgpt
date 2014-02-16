64c73363ce37f7087bcf2abef41c0525ec724246
shenpengru <shenpengru@gmail.com>
2014-02-16 17:19:49 +0800

    modify

--- getgpt.c
+++ getgpt.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define DEBUG (0)
+#define DBG if(DEBUG)printf
+
 #define MMCDEV 		"/dev/block/mmcblk0"
 
 /*
@@ -41,15 +44,45 @@ enum {
 	ENUM_BLKADDR_PRIGPT_ENTRY_END	= 33
 };
 
+void dump_mem(char* name, char* mem, int size)
+{
+	int i;
+	int addr = 0;
+
+	DBG("\n%s\n", name);
+	DBG("-----------------------------------------------------------\n");
+	for(i=0; i<size; i++){
+		if( i%16==0 ){
+			DBG("(0x%04X) ", addr);
+			addr += 16;
+		}
+		DBG(" %02X", mem[i]&0xFF );
+		if( i%8==7 ){
+			DBG("  ");
+		}
+		if( i%16==15 ){
+			DBG("\n");
+		}
+	}
+	DBG("\n");
+}
+
 struct gpt_header {
 	char sig_name[8];
-	
-};
+	int version[4];
+	int header_size[4];
+	int pri_head_addr;
+	int sec_head_addr;
+	int part_start;
+	int part_end;
+}gGPTPriHeader, gGPTSecHeader;
 
 int check_if_efi(char* gptheader)
 {
 	int i;
 	char efi_sig[8] = {0x45, 0x46, 0x49, 0x20, 0x50, 0x41, 0x52, 0x54}; // "EFI PART"
+
+	dump_mem("primary gpt header", gptheader, 96);
 
 	for(i=0; i<8; i++){
 		if( gptheader[i] != efi_sig[i] ){
@@ -57,7 +90,23 @@ int check_if_efi(char* gptheader)
 			return 1;
 		}
 	}
+
+	int part_can_use=0;
+	for(i=0; i<4; i++){
+		gGPTPriHeader.pri_head_addr |= ((gptheader[24+i]&0xFF)<<(i*2*4));
+		gGPTPriHeader.sec_head_addr |= ((gptheader[32+i]&0xFF)<<(i*2*4));
+		gGPTPriHeader.part_start    |= ((gptheader[40+i]&0xFF)<<(i*2*4));
+		gGPTPriHeader.part_end      |= ((gptheader[48+i]&0xFF)<<(i*2*4));
+	}
+	part_can_use = gGPTPriHeader.part_end - gGPTPriHeader.part_start +1;
 	
+	printf("\n");
+	printf("  Primary GPT header block address : 0x%08X(%d)\n", gGPTPriHeader.pri_head_addr, gGPTPriHeader.pri_head_addr);
+	printf("Secondary GPT header block address : 0x%08X(%d)\n", gGPTPriHeader.sec_head_addr, gGPTPriHeader.sec_head_addr);
+	printf("     Partition start block address : 0x%08X(%d)\n", gGPTPriHeader.part_start, gGPTPriHeader.part_start);
+	printf("                 end block address : 0x%08X(%d)\n", gGPTPriHeader.part_end, gGPTPriHeader.part_end);
+	printf("                     block can use : 0x%08X(%d)\n\n", part_can_use, part_can_use);
+
 	return 0;
 }
 
